c5ae4cb1cdb2801c6e58de7b4e5a935c3347ee44
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2024-05-17 22:13:20 -0400

    test npc dialogue flow integrated

--- Assets/Scripts/NPC/NPCInteraction.cs
+++ Assets/Scripts/NPC/NPCInteraction.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class NPCInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string startNode = "Test";
+    private bool playerInRange = false;
+    private DialogueRunner dialogueRunner;
+    public KeyCode interactionKey = KeyCode.E;
+
+    private void Start()
     {
-        
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+        Debug.Log($"DialogueRunnerFound: {dialogueRunner}");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerInRange && Input.GetKeyDown(interactionKey))
+        {
+            StartDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void StartDialogue()
+    {
+        dialogueRunner.StartDialogue(startNode);
     }
 }
