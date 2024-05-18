015931485cf5cb6479c514501e7b38f5183ecaf0
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2024-05-18 13:43:41 -0400

    skill level refactor

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -7,17 +7,24 @@ public class PlayerStats : MonoBehaviour
     public Stat SoulPoints;
     public Stat DivinationSkill;
     public Stat GatheringSkill;
+    public Stat MovementSpeed;
     Subscription<DivinationEvent> divination_event_subscription;
     Subscription<PickupEvent> pick_up_event_subscription;
 
     private void Awake()
     {
+        // Initialize playerStats
         SoulPoints = new Stat("soul points", 0, 100, 100);
-        DivinationSkill = new Stat("divination ability", 0, 0, 1000);
-        GatheringSkill = new Stat("gethering skill", 0, 0, 1000);
+        DivinationSkill = new Stat("divination ability", 0, 1000, 0);
+        GatheringSkill = new Stat("gethering skill", 0, 1000,0);
+        MovementSpeed = new Stat("movement speed", 2, 5, 5);
+
+        // Event Subscription
         divination_event_subscription = EventBus.Subscribe<DivinationEvent>(DivinationStatChange);
         pick_up_event_subscription = EventBus.Subscribe<PickupEvent>(GatheringSkillStatChange);
+        
     }
+
     private void Start()
     {
        
@@ -36,10 +43,12 @@ public class PlayerStats : MonoBehaviour
     {
         DivinationSkill.IncreaseCurrentValue(e.proficiencyIncrease);
         Debug.Log($"Divination Skill: {DivinationSkill.currVal}");
+        // TODO handle skill points + leveling up
+
         SoulPoints.DecreaseCurrentValue(e.soulPointsDecrease);
         Debug.Log($"Soul Points: {SoulPoints.currVal}");
+        // TODO handle skill points + leveling up
     }
-
 
     /// <summary>
     /// increases gathering proficiency
@@ -49,6 +58,8 @@ public class PlayerStats : MonoBehaviour
     {
         GatheringSkill.IncreaseCurrentValue(e.proficiencyIncrease);
         Debug.Log($"Gathering Skill: {GatheringSkill.currVal}");
+
+        // TODO hange skill points + leveling up
     }
 }
 
