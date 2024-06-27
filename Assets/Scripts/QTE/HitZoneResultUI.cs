62b2de130a7c0008cc551c3636ee59929b5a4808
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2024-06-26 22:30:03 -0400

    new font

---
+++ Assets/Scripts/QTE/HitZoneResultUI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class HitZoneResultUI : MonoBehaviour
+{
+    private TextMeshProUGUI textMeshPro;
+    Subscription<HitZoneResultEvent> hit_zone_result_subscription;
+    Subscription<QteEndEvent> qte_end_subscription;
+    [SerializeField]
+    List<float> resultList;
+    float overallAccuracy;
+
+    private void Start()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+        hit_zone_result_subscription = EventBus.Subscribe<HitZoneResultEvent>(UpdateResultUI);
+        qte_end_subscription = EventBus.Subscribe<QteEndEvent>(ShowFinalResult);
+    }
+
+    void UpdateResultUI(HitZoneResultEvent e)
+    {
+        //Update the current display
+        textMeshPro.text = e.message;
+        //Update reuslt list for final calculation;
+        if(e.message.Length != 0)
+        {
+            resultList.Add(e.accuracy);
+        }
+    }
+
+    void ShowFinalResult(QteEndEvent e)
+    {
+        string msg = "";
+        foreach (float result in resultList)
+        {
+            overallAccuracy += result;
+        }
+
+        overallAccuracy /= resultList.Count;
+        overallAccuracy *= 100;
+        double roundedAccuracy = Math.Round(overallAccuracy, 2);
+        Debug.Log($"overall accuracy {roundedAccuracy}%");
+
+       
+        if(overallAccuracy > 80)
+        {
+            msg = $"Perfect Divination \n You achieved {roundedAccuracy}% on accuracy.";
+            EventBus.Publish(new DivinationResultIndexEvent(0));
+        }
+
+        else if (overallAccuracy > 60)
+        {
+            msg = $"Excellent Divination \n You achieved {roundedAccuracy}% on accuracy.";
+            EventBus.Publish(new DivinationResultIndexEvent(1));
+        }
+
+        else if (overallAccuracy > 40)
+        {
+            msg = $"Average Divination \n You achieved {roundedAccuracy}% on accuracy.";
+            EventBus.Publish(new DivinationResultIndexEvent(2));
+        }
+
+        else
+        {
+            msg = $"Failed Divination \n You achieved {roundedAccuracy}% on accuracy.";
+            EventBus.Publish(new DivinationResultIndexEvent(3));
+        }
+
+        textMeshPro.text = msg;
+    }
+}
